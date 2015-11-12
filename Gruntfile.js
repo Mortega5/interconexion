894bba2c8291a257bc9e04af3fb267298a773040
Miguel Ortega Moreno <miguel.ortega.moreno5@gmail.com>
2015-11-12 10:21:49 +0100

    Added grunt tasks for uglify scrips and images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+/*global module*/
 module.exports = function(grunt) {
   grunt.initConfig({
     ngdocs: {
@@ -17,11 +18,12 @@ module.exports = function(grunt) {
     /* Watch the changes of the files (in this case, scrips) and execute the task*/
     watch: {
       options: {
-        livereload: true
+        livereload: true,
+        spawn: false
       },
       scripts: {
-        files: ["app/scripts/*.js", "Gruntfile.js"],
-        tasks: ["ngdocs"]
+        files: ["app/scripts/*.js", "Gruntfile.js", "app/images/*.{png,jpg,gif}"],
+        tasks: ["ngdocs", "newer:imagemin"]
       }
     },
     /* Create a new server in the port 8080*/
@@ -33,20 +35,72 @@ module.exports = function(grunt) {
         }
       }
     },
-    /* Open a new tab in the default navigator*/
-    open: {
-      all: {
-        path: "http://localhost:<%= connect.all.options.port%>/docs"
+
+    useminPrepare: {
+      html: "app/index.html",
+      options: {
+        dest: "."
       }
-    }
+    },
+    usemin: {html: ["app/build/index.html"]},
+    uglify: {
+      minControllers: {
+        files: {
+          "app/scripts/app.min.js": ["app/scripts/*.js", "!app/scripts/*.min.js"]
+        }
+      }
+    },
+    copy: {
+      task0: {
+        src: "app/index.html",
+        dest: "app/build/index.html"
+      }
+    },
+    imagemin: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: "images",
+          src: ["*.{png,jpg,gif,.svg}"],
+          dest: "app/images/min"
+        }]
+      }
+    },//imagemi
+
+    images: {
+
+      files: ["app/images//*.{png,jpg,gif}"],
+      tasks: ["newer:imagemin"],
+      options: {
+        spawn: false
+      }
+    },
+
+    /* Limpia los ficheros temporales del concat */
+    clean: [".tmp"]
   });
 
   grunt.loadNpmTasks("grunt-ngdocs");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.loadNpmTasks("grunt-open");
-  grunt.loadNpmTasks("grunt-contrib-livereload");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-newer");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-usemin");
+  grunt.loadNpmTasks("grunt-contrib-clean");
 
   grunt.registerTask("default", "ngdocs");
   grunt.registerTask("server", ["connect", "ngdocs", "watch"]);
+  grunt.registerTask("build", [
+    "copy:task0",
+    "useminPrepare",
+    "concat",
+    "uglify",
+    "usemin",
+    "clean"
+  ]);
 };
+
+
